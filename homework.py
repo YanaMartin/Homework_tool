586a154e90f2066f4f43045fc2c9048be87a2227
YanaMartin <yanakirilove@gmail.com>
2023-12-21 10:50:41 +0100

    home page correct

--- homework.py
+++ homework.py
@@ -1,30 +1,22 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from config import Config
-from forms import LoginForm
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#@app.route("/")
-#def hello():
-#    return "Hello World!"
-
-#@app.route("/PyLadies")
-#def PyLadies():
-#    return "PyLadies Flask"
-
-
-# @app.route('/login')
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/PyLadies')
+def submit_homework():
+    """Home page with form to submit homework"""
+    if request.method == 'POST':
+        name = request.form['name']
+        lesson_number = request.form['lesson_number']
+        colab_url = request.form['colab_url']
+        comments = request.form['comments']
+
+        return redirect('/thank-you')
     return render_template('name.html', title='Homework submission', form=form)
+
 if __name__ == "__main__":
     app.run()
