632fafbb7ba525aa21ea6b03a08f226a8e6e1a3d
YanaMartin <yanakirilove@gmail.com>
2023-12-21 10:55:19 +0100

    correct home page

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ def submit_homework():
         comments = request.form['comments']
 
         return redirect('/thank-you')
-    return render_template('name.html', title='Homework submission', form=form)
+    return render_template('name.html')
 
 if __name__ == "__main__":
     app.run()
