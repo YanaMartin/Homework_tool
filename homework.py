a5601f6edaecc8e37fb532bf9809f4d8522695b7
YanaMartin <yanakirilove@gmail.com>
2023-12-21 11:07:56 +0100

    home page and thank you

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from config import Config
 
 app = Flask(__name__)
@@ -15,8 +15,13 @@ def submit_homework():
         colab_url = request.form['colab_url']
         comments = request.form['comments']
 
-        return redirect('/thank-you')
+        return redirect(url_for('thank_you'))
     return render_template('name.html')
+
+@app.route('/thank-you')
+def thank_you():
+    """Thank you page after submitting homework succesfully"""
+    return render_template('thank_you.html')
 
 if __name__ == "__main__":
     app.run()
