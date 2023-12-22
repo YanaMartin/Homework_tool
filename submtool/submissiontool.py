902a3d58695d7608a1dbfcd6e210572c94e02407
dani limbeck <dani.limbeck@gmx.at>
2023-12-22 23:45:34 +0100

    interface (html, css)

--- submtool/submissiontool.py
+++ submtool/submissiontool.py
@@ -5,14 +5,17 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+#Function to write data to a CSV file with timestamp
+
 def write_to_csv(data):
-    """Function to write data to a CSV file with timestamp"""
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    data_with_timestamp = [timestamp] + data
+    data_with_timestamp = data + [timestamp]
     with open('homework_data.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(data_with_timestamp)
+    file.close()
+
 
 @app.route("/submtool")
 def hello():
@@ -30,17 +33,18 @@ def data():
         comments = request.form['comments']
         data = [name, lesson_number, colab_url, comments]
         write_to_csv(data)
-        return render_template('data.html', formdata = formdata)
-
+        return render_template('data.html', data = formdata)
+        
+#Page to view all submitted homework data from CSV
+    
 @app.route('/view-homework')
 def view_homework():
-    """Page to view all submitted homework data"""
     data = []
     with open('homework_data.csv', mode='r') as file:
         reader = csv.reader(file)
         for row in reader:
             data.append(row)
-    return render_template('view_homework.html', homework=data)
+    return render_template('view_homework.html', homework = data)
 
 if __name__ == "__main__":
     app.run()
